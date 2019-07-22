8a9f39abf14a1950a20a0b23f0876a4289c895fb
amackillop <austin.mackillop@gmail.com>
2019-07-22 19:39:20 -0400

    Changed main to take args

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
     
-import           Data.Text                     as T
+-- import           Data.Text                     as T
 import           System.Environment
 import           Parsing
 
 main :: IO ()
-main = getLine >>= putStr . readExpr
+main = getArgs >>= putStr . readExpr . head
--- src/Parsing.hs
+++ src/Parsing.hs
@@ -8,16 +8,20 @@ import           Text.ParserCombinators.Parsec
 
 import qualified Data.Text                     as T
 import qualified Data.Char                     as C
+import qualified Data.Vector                   as V
 import           Numeric
 
 data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
+             | Vector (V.Vector LispVal)
              | Number Integer
              | Float Float
              | Character Char
              | String String
-             | Bool Bool deriving Show
+             | Bool Bool
+
+instance Show LispVal where show = showVal
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:"
@@ -31,9 +35,9 @@ escaped = tab <|> newline <|> (string "\\" >> oneOf "r\\\"")
 radixNum :: Parser Integer
 radixNum = do
   char '#'
-  base <- oneOf "bodx"
+  base   <- oneOf "bodx"
   digits <- many1 $ parser base
-  return $ reader base digits
+  return $ fst $ head $ reader base digits
  where
   parser :: Char -> Parser Char
   parser b = case b of
@@ -41,21 +45,18 @@ radixNum = do
     'o' -> octDigit
     'd' -> digit
     'x' -> hexDigit
-  reader :: Char -> String -> Integer
+  reader :: Char -> String -> [(Integer, String)]
   reader b = case b of
-    'b' -> fst . head . readInt 2 (`elem` "01") C.digitToInt
-    'o' -> fst . head . readOct
-    'd' -> read
-    'x' -> fst . head . readHex
+    'b' -> readInt 2 (`elem` "01") C.digitToInt
+    'o' -> readOct
+    'd' -> readDec
+    'x' -> readHex
 
 -- Add floats, complex numbers etc.
 
 parseString :: Parser LispVal
-parseString = do
-  char '"'
-  x <- many (escaped <|> noneOf "\"")
-  char '"'
-  return $ String x
+parseString =
+  String <$> (char '"' *> many (escaped <|> noneOf ['"']) <* char '"')
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -71,13 +72,61 @@ parseNumber :: Parser LispVal
 parseNumber = (Number . read <$> many1 digit) <|> (Number <$> radixNum)
 
 parseExpr :: Parser LispVal
-parseExpr = parseNumber <|> parseAtom <|> parseString
+parseExpr =
+  try parseNumber
+    <|> parseString
+    <|> parseQuoted
+    <|> do
+          char '('
+          x <-
+            try parseList
+            <|> try parseDottedList
+            <|> try parseBackQuotes
+            <|> try parseVector
+          char ')'
+          return x
+    <|> parseAtom
+
+inParens :: Parser a -> Parser a
+inParens parser = char '(' *> parser <* char ')'
+
+sepBySpaces :: Parser Char -> Parser String
+sepBySpaces parser = sepBy parser spaces
 
 parseList :: Parser LispVal
-parseList = List <$> sepBy parseExpr spaces
+parseList = List <$> inParens (sepBy parseExpr spaces)
 
-parse
+parseDottedList :: Parser LispVal
+parseDottedList = inParens $ do
+  head <- endBy parseExpr spaces
+  tail <- char '.' *> spaces *> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted =
+  char '\'' >> parseExpr >>= (\x -> return $ List [Atom "quote", x])
+
+parseBackQuotes :: Parser LispVal
+parseBackQuotes =
+  char '`' >> parseExpr >>= (\x -> return $ List [Atom "backquote", x])
+
+parseVector :: Parser LispVal
+parseVector = Vector . V.fromList <$> inParens (sepBy parseExpr spaces)
+
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
   Left  err -> "No match: " ++ show err
   Right val -> "Found value: " ++ show val
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom   name    ) = name
+showVal (Number contents) = show contents
+showVal (Bool   True    ) = "#t"
+showVal (Bool   False   ) = "#f"
+showVal (List   contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) =
+  "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
