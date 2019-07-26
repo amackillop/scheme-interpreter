c1772b934abfa7bdc0ddf07f0cc4ecf1158a01d2
amackillop <austin.mackillop@gmail.com>
2019-07-25 20:13:35 -0400

    Repl ignore Ctrl-C and exits gracefully with ctrl d

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,7 @@
--- {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Main where
 
--- import           Data.Text                     as T
 import           System.Environment
 import           System.Console.Haskeline
 import           System.IO                      ( hFlush
@@ -11,14 +9,18 @@ import           System.IO                      ( hFlush
                                                 )
 import           Control.Monad                  ( unless
                                                 , forever
+                                                , void
                                                 )
 
 import           Parse
 import           Eval
 
 main :: IO ()
-main = runInputT defaultSettings $ forever $ getInputLine "scheme > " >>= \case
-  Nothing      -> return ()
-  Just ":q"    -> return ()
-  Just ":quit" -> return ()
-  Just input   -> outputStrLn $ show . eval . readExpr $ input
+main = runInputT defaultSettings $ loop
+ where
+  loop =  ignoreCtrlC $ getInputLine "scheme > " >>= \case
+    Nothing    -> void $ outputStrLn "Moriturus te saluto."
+    Just ""    -> loop
+    Just input -> outputStrLn (show . eval . readExpr $ input) >> loop
+  ignoreCtrlC = handleInterrupt loop . withInterrupt
+
